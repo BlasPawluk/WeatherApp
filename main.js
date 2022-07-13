5b6ef10bc68ce797df401b6f7825e1a7a8a1bb31
Blas Pawluk <blaspaw99@gmail.com>
2022-07-13 11:09:58 -0300

    cambiar ciudades por consola

---
+++ main.js
@@ -0,0 +1,27 @@
+let weather = {
+  apiKey: 'ae48f79affb5a83d75b4c417f3c80491',
+  fetchWeather: function (ciudad) {
+    fetch(
+      'https://api.openweathermap.org/data/2.5/weather?q=' +
+        ciudad +
+        '&units=metric&appid=' +
+        this.apiKey
+    )
+      .then((response) => response.json())
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    console.log(name, icon, description, temp, humidity, speed);
+    document.querySelector('.city').innerText = 'Temperatura en ' + name;
+    document.querySelector('.icon').src =
+      'https://openweathermap.org/img/wn/' + icon + '.png';
+    document.querySelector('.description').innerText = description;
+    document.querySelector('.temp').innerText = temp + '°C';
+    document.querySelector('.humidity').innerText = humidity + '%';
+    document.querySelector('.wind').innerText = speed + 'km/h';
+  },
+};
