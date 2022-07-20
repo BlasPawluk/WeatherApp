c158532ad15a79b8cdaba63022dcd02b591b9721
Blas Pawluk <blaspaw99@gmail.com>
2022-07-20 01:04:42 -0300

    Proyect Ready

--- main.js
+++ main.js
@@ -10,7 +10,7 @@ let weather = {
       .then((response) => response.json())
       .then((data) => {
         this.displayWeather(data);
-        console.log(data);
+        console.log('data', data);
       });
   },
   displayWeather: (data) => {
@@ -18,7 +18,7 @@ let weather = {
     const { icon, description } = data.weather[0];
     const { temp, humidity } = data.main;
     const { speed } = data.wind;
-    console.log(name, icon, description, temp, humidity, speed);
+
     document.querySelector('.city').innerText = 'Weather in: ' + name;
     document.querySelector('.icon').src =
       'https://openweathermap.org/img/wn/' + icon + '.png';
